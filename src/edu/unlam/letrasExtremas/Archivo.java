fa52b5168b7fcd395cd39bd20121aaaf854965dd
joniborda <jonathanmatiasborda@gmail.com>
2018-08-25 18:32:15 -0300

    leer archivo

--- src/edu/unlam/letrasExtremas/Archivo.java
+++ src/edu/unlam/letrasExtremas/Archivo.java
@@ -1,11 +1,48 @@
 package edu.unlam.letrasExtremas;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Archivo {
 
 	public String name;
+	public String contenido;
+	private BufferedReader entradaBytes;
+	private BufferedReader lector;
 
 	public Archivo(String name) {
 		this.name = name;
+		this.contenido = "";
+
+		InputStream entradaBytes = ClassLoader.class.getResourceAsStream(this.name);
+
+		this.lector = new BufferedReader(new InputStreamReader(entradaBytes));
+
+	}
+
+	public String leerSiguienteLinea() {
+		String linea = null;
+		try {
+			if ((linea = this.lector.readLine()) != null) {
+				return linea;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (this.entradaBytes != null) {
+					this.entradaBytes.close();
+				}
+				if (this.lector != null) {
+					this.lector.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 	/**
--- src/edu/unlam/letrasExtremas/LetrasExtremas.java
+++ src/edu/unlam/letrasExtremas/LetrasExtremas.java
@@ -6,5 +6,16 @@ public class LetrasExtremas {
 
 	private int n = 0;
 	private ArrayList<String> palabras;
+	
+	private String ruta_archivo;
+	private Archivo archivo;
+	
+	public LetrasExtremas(int n, ArrayList<String> palabras, String ruta_archivo) {
+		this.n = n;
+		this.palabras = palabras;
+		this.ruta_archivo = ruta_archivo;
+		this.archivo = new Archivo("empate.in");
+	}
+	
 
 }
