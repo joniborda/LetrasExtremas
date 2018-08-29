d668aba14c9a6f03f05b0bb18f3d7d3f998af9af
joniborda <jonathanmatiasborda@gmail.com>
2018-08-29 00:15:11 -0300

    Arreglos a Nico en ganadoras
    Mejoro la logica para guardar las letras ganadoras y las palabras ganadoras.

--- src/edu/unlam/letrasExtremas/LetrasExtremas.java
+++ src/edu/unlam/letrasExtremas/LetrasExtremas.java
@@ -6,8 +6,9 @@ public class LetrasExtremas {
 
 	private Integer n = 0;
 	private ArrayList<String> palabras;
-	private static final int CANTIDAD_LETRAS = 26;
-	private int matLetras[][] = new int[2][CANTIDAD_LETRAS];
+	private static final int CANTIDAD_LETRAS = 25;
+	private static final int ASCII_INICIAL = 97;
+	private int matLetras[] = new int[CANTIDAD_LETRAS];
 	private ArrayList<Character> letrasGanadoras;
 	private ArrayList<String> palabrasGanadoras;
 
@@ -31,83 +32,59 @@ public class LetrasExtremas {
 	public void contarLetrasExtremas() {
 		String palabra = null;
 
-		for (int i = 0; i < this.n; i++) { // for que recorre el arrayList de
-											// palabras
-			palabra = this.palabras.get(i); // me traigo la palabra
+		for (int i = 0; i < this.n; i++) {
+			palabra = this.palabras.get(i);
 
 			// Guardo todas las letras en la posición ASCII - 'a' (97) para que
-			// me queden
-			// ordenadas en mi matriz
-			// en la segunda fila (fila 1) guardo la cantidad de apariciones de
-			// todas las
-			// letras
+			// me queden ordenadas
 
 			// primer letra de la palabra leida
-			int primera_letra = (int) palabra.charAt(0) - 97;
-
-			if (this.matLetras[0][primera_letra] == 0) {
-				// si aún no hay una letra cargada guardo la primera
-				this.matLetras[0][(int) palabra.charAt(0) - 97] = (int) palabra.charAt(0);
-				this.matLetras[0][primera_letra] = (int) palabra.charAt(0);
-			} // la
-				// primera
-			this.matLetras[1][primera_letra]++;
+			this.matLetras[(int) palabra.charAt(0) - ASCII_INICIAL]++;
 
 			// ultima letra de la palabra leida
-			int ultima_letra = (int) palabra.charAt(palabra.length() - 1) - 97;
-			if (this.matLetras[0][ultima_letra] == 0) {
-				this.matLetras[0][ultima_letra] = (int) palabra.charAt(palabra.length() - 1);
-			}
-
-			this.matLetras[1][ultima_letra]++;
+			this.matLetras[(int) palabra.charAt(palabra.length() - 1) - ASCII_INICIAL]++;
 
 		}
 
 		this.letrasGanadoras = new ArrayList<Character>();
 		this.palabrasGanadoras = new ArrayList<String>();
-		// Debo buscar la o las letras ganadoras. Recorro la matriz de letras
-		this.letrasGanadoras.add((char) this.matLetras[0][0]);
-		int cantRepeLetra = this.matLetras[1][0]; // guardo la primer letra y la
-													// cantidad de repeticiones
 
-		for (int i = 1; i < 25; i++) {
-			if (this.matLetras[1][i] > cantRepeLetra) {
+		// Busco las letras con mas repeticiones
+		int maximaCantidadRepetida = 0;
+		for (int i = 0; i < CANTIDAD_LETRAS; i++) {
+			if (this.matLetras[i] > maximaCantidadRepetida) {
 				this.letrasGanadoras.clear();
-				this.letrasGanadoras.add((char) this.matLetras[0][i]);
-				cantRepeLetra = this.matLetras[1][i];
-			} else if (this.matLetras[1][i] == cantRepeLetra)
-				this.letrasGanadoras.add((char) this.matLetras[0][i]);
-
+				this.letrasGanadoras.add((char) ((char) i + ASCII_INICIAL));
+				maximaCantidadRepetida = this.matLetras[i];
+			} else if (maximaCantidadRepetida != 0 && this.matLetras[i] == maximaCantidadRepetida) {
+				this.letrasGanadoras.add((char) ((char) i + ASCII_INICIAL));
+			}
 		}
 
-		// Debo buscar la o las palabras ganadoras. Recorro el arrayList de
-		// palabras
+		// A partir de las letras guardar las palabras ganadoras
+		boolean esPalabraGanadora;
 		for (int i = 0; i < this.palabras.size(); i++) {
 			palabra = this.palabras.get(i);
 
-			for (int j = 0; j < this.letrasGanadoras.size(); j++) {
+			int j = 0;
+			esPalabraGanadora = false;
+			while (!esPalabraGanadora && j < this.letrasGanadoras.size()) {
 
 				if (palabra.charAt(0) == this.letrasGanadoras.get(j)
 						|| palabra.charAt(palabra.length() - 1) == this.letrasGanadoras.get(j)) {
-
-					// busco si la palabra está repetida este while busca la
-					// palabra analizada dentro de las ganadoras, si ya está
-					// sale del while, caso contrario la busca hasta el final.
-					// Como tiene el operador &&, con solo verificar una
-					// condicion ya es suficiente para relizar el corte.
-
-					int k = 0;
-					while (k < this.palabrasGanadoras.size() && this.palabrasGanadoras.get(k) != palabra) {
-						k++;
-					}
-
-					// si el K sale con la cantidad de elementos del
-					// arraylist es porque no encontró la palabra
-					if (k == this.palabras.size()) {
-						this.palabrasGanadoras.add(palabra);
-					}
+					this.palabrasGanadoras.add(palabra);
+					esPalabraGanadora = true;
 				}
+				j++;
 			}
+		}
+
+		for (int i = 0; i < this.letrasGanadoras.size(); i++) {
+			System.out.println(this.letrasGanadoras.get(i));
+		}
+
+		for (int i = 0; i < this.palabrasGanadoras.size(); i++) {
+			System.out.println(this.palabrasGanadoras.get(i));
 		}
 
 	}
